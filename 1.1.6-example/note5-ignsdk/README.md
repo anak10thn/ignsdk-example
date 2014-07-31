37926b754b4df0d6e01b7a421b86213992534e1d
Ibnu Yahya <anak10thn@gmail.com>
2014-07-31 16:49:23 +0800

    ADD: note5 for ignsdk

---
+++ 1.1.6-example/note5-ignsdk/README.md
@@ -0,0 +1,20 @@
+note5
+=====
+
+note5 is a HTML5 Plain Text Editor. It implements HTML5 File API, specifically FileReader and Blob.
+
+##Compatible Web Browser##
+* Chrome (including WebKit based web browser)
+
+##Features##
+* Open file
+* Save file (download)
+
+##Screenshots##
+![note5](http://html5.fitraditya.com/note5/screenshot.png)
+
+##What's Next##
+* Others web browser compatibilty
+
+##Maintainer##
+* Fitra Aditya (fitra@g.pl)
---
+++ 1.1.6-example/note5-ignsdk/assets/js/note5.js
@@ -0,0 +1,81 @@
+var fs = ign.filesystem();
+
+// Load file function
+var loadFile = function() {
+  var path = $('#inputFileName').val();
+  $('#newFileName').val(path);
+  var fileBuffer = fs.fileRead(path);
+  $('#text-area').val(fileBuffer);
+  $('#text-area').change();
+  if ($('#text-area')[0].scrollHeight > $('#text-area').height()) {
+    $('#text-area').css('height', 'auto');
+    $('#text-area').height($('#text-area')[0].scrollHeight);
+  }
+}
+
+// Save file function
+var saveFile = function() {
+  var filepath = $('#newFileName').val();
+  var text = $('#text-area').val();
+  fs.fileWrite(filepath,text);
+}
+
+// Undo / Redo function
+var formatText = function(act, sel) {
+  document.execCommand(act, false, sel);
+}
+
+$(function() {
+
+  // Window properties
+  var win = $(window);
+  var w_win = win.width();
+  var h_win = win.height();
+
+  // Text area properties
+  var t = $('#text-area');
+  var w_t = w_win;
+  var h_t = h_win;
+
+  // Set wrapper, line number, and text area height x width for the first time
+  t.isEditable = true;
+  w_t = w_win - 5;
+  h_t = h_win - 70;
+  t.css('width', w_t);
+  t.css('height', h_t);
+
+  // Set wrapper, line number, and text area height x width when window resized
+  win.resize(function() {
+    w_win = win.width();
+    h_win = win.height();
+    w_t = w_win - 5;
+    h_t = h_win - 70;
+    t.css('width', w_t);
+    t.css('height', h_t);
+  });
+
+  // Customize input file
+  $('#openFile').click(function() {
+    var path = fs.openFileDialog();
+    $('#inputFileName').val(path);
+  });
+
+  // Load file action
+  $('#loadFile').on('click', '', function() {
+    loadFile();
+  });
+
+  // Save file action
+  $('#saveFile').on('click', '', function() {
+    saveFile();
+  });
+
+  // Undo / Redo action
+  $('#undoAction').on('click', '', function() {
+    formatText('undo');
+  });
+  $('#redoAction').on('click', '', function() {
+    formatText('redo');
+  });
+
+});
